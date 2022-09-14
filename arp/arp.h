d32e8f81d2647ad3eefa886add589ae81da46330
rodrigodelama <rodrigo.delama@icloud.com>
2022-09-14 20:03:14 +0200

    added arp_header struct

--- arp/arp.h
+++ arp/arp.h
@@ -1,8 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 struct arp_header
 {
     uint8_t hw_type;
-    uint8_t proto_type;
-
-    ipv4_addr_t xx_target;
-}
+    uint8_t protocol_type; //IPv4
+    uint8_t hw_size;
+    uint8_t protocol_size;
+    uint8_t opcode; //request (1) or reply (2)
+    mac_addr_t sndr_MAC; // format XX:XX:XX:XX:XX:XX
+    ipv4_addr_t sndr_IP; // format xxx.xxx.xxx.xxx, where x is any int [0,255]
+    mac_addr_t target_MAC;
+    ipv4_addr_t target_IP;
+};
