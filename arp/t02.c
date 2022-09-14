87cbea867e69b920c8b60a9c1b14dfbd61e13b48
Pablo Ramos <cm2001xd@gmail.com>
2022-09-14 10:44:39 +0200

    Create t02.c

---
+++ arp/t02.c
@@ -0,0 +1,23 @@
+
+Estructura programa: 
+//Creamos la estructura con los campos correspondientes struct arp_frame{};
+//Hacemos ARP_resolve int arp_resolve(eth_iface_t * iface,ipv4_addr_t ip_addr, ipv4_addr_t 
+ip_addr_src, mac_addr_t mac_addr){ 
+--------------------------------------------------------------------------------
+//Estructura función 
+arp_resolve() int arp_resolve(eth_iface_t * iface,ipv4_addr_t ip_addr, ipv4_addr_t 
+ip_addr_src, mac_addr_t mac_addr){ 
+//Creamos variables auxiliares 
+//Rellenamos la estructura con los datos correspondientes
+//Comprobamos que la mac introducida se corresponde con una mac 
+y=mac_str_addr(dest_MAC,arp_frame.dest_addr_mac); 
+// Abrimos la interfaz Ethernet //
+//Enviamos tramas Ethernet 
+// Inicializamos el temporizador para mantener timeout si se reciben tramas con tipo incorrecto.
+do{
+//Recibimos tramas Ethernet
+//Comprobamos que tenga un tamaño correcto 
+// Comprobamos si es la trama que estamos buscando 
+}while (); 
+//Hasta que sea la trama buscada
+}
