84db399c3e1882a447a19aeb12ac56601352e8a5
Rodrigo De Lama <rodrigo.delama@icloud.com>
2022-09-14 10:20:24 +0200

    latest

---
+++ arp/arp.c
@@ -0,0 +1,7 @@
+int arp_resolve(eth_iface_t * iface, ipv4_addr_t ip_addr, mac_addr_t mac_addr)
+{
+    //ARP - Address Resolution Protocol
+
+    //announce your IP when asked to your MAC Address (or the other way around...)
+}
+
