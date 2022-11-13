19ddae5efb13c4427788cdd1fe25994768c7b760
rodrigodelama <rodrigo.delama@icloud.com>
2022-11-13 18:50:56 +0100

    latest

--- ripv2_client.c
+++ ripv2_client.c
@@ -2,3 +2,14 @@
 #include "global_dependencies.h"
 
 //only recieve operations
+
+/*
+
+Enviar una petición de toda la tabla de entradas
+El programa recibe como parámetros por línea de comandos:
+Ficheros de configuración IPv4
+Dirección IPv4 del servidor/router
+Imprime por pantalla los RTEs (vectores distancia) recibidos
+Probad primero contra los router
+
+*/
--- ripv2_server.c
+++ ripv2_server.c
@@ -2,3 +2,24 @@
 #include "global_dependencies.h"
 
 //only send operations
+
+/*
+
+Generar el response (toda la tabla):
+Crear cabecera IPv4+UDP con:
+ipv4.destino = 10.0.1.1 (IPv4 del router del request)
+udp.puerto_destino = puerto origen del request / 520 (RIP port) para periodic responses
+udp.puerto_origen = 520 (RIP port)
+Rellenar las entradas necesarias en el mensaje RIPv2:
+ripv2.command = 2 (response)
+ripv2.version = 2 (RIPv2)
+ripv2.dom_encam = 0x0000
+ripv2.RTE[i].familia_de_direcciones = 2 (IP)
+ripv2.RTE[i].dirección= 10.0.30.0 (ejemplo)
+ripv2.RTE[i].máscara = 255.255.255.0 (ejemplo)
+ripv2.RTE[i].siguiente_salto = 0.0.0.0
+ripv2.RTE[i].métrica = 1
+
+32 bits métrica -> hacer htonl()
+
+*/
